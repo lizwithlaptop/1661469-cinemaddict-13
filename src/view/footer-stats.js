27d56e67fb1e884993f97829bb812a4996ba3806
Liza Ananeva <liza.ananeva.r@gmail.com>
2021-01-21 22:42:55 +0300

    Время разбивать камни

---
+++ src/view/footer-stats.js
@@ -0,0 +1,6 @@
+export const createFooterStatsTemplate = () => {
+    return `<section class="footer__logo logo logo--smaller">Cinemaddict</section>
+        <section class="footer__statistics">
+            <p>130 291 movies inside</p>
+        </section>`
+}
